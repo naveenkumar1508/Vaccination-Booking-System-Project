a305bd94f05d80704a1a99e56631bbfbdf8aaf30
Naveenkumar <naveent1508@gmail.com>
2025-08-01 15:53:21 +0530

    Vaccination Booking System Project Structure

---
+++ VaccineNationBookingSystem/src/main/java/com/example/VaccineNationBookingSystem/VaccineNationBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.VaccineNationBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccineNationBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccineNationBookingSystemApplication.class, args);
+	}
+
+}
---
+++ VaccineNationBookingSystem/src/test/java/com/example/VaccineNationBookingSystem/VaccineNationBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaccineNationBookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccineNationBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
